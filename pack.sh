5b9d7e37164c4adeb7e8a046d60b93bf4f46c8e5
zhiyongli <iamlizhiyong@outlook.com>
2023-09-15 16:26:01 +0800

    Add Debian

--- pack.sh
+++ pack.sh
@@ -2,3 +2,7 @@ pack buildpack package showpune/buildpack-java-azure:latest --config ./package.t
 docker push showpune/buildpack-java-azure:latest
 pack builder create showpune/builder-java-azure:jammy --config ./builder.toml
 docker push showpune/builder-java-azure:jammy
+
+
+pack builder create showpune/builder-java-azure:debian --config ./builder.toml
+docker push showpune/builder-java-azure:debian
--- test.sh
+++ test.sh
@@ -1,1 +1,4 @@
-pack build showpune/spring-petclinic:mybp --builder showpune/builder-java-azure:jammy --path /mnt/c/projects/spring-petclinic -v
+pack build showpune/spring-petclinic:mybp --builder showpune/builder-java-azure:debian --path /mnt/c/projects/spring-petclinic -v
+docker run --rm -p 8080:8080 showpune/spring-petclinic:mybp-debian
+
+pack rebase showpune/spring-petclinic:mybp-debian 
