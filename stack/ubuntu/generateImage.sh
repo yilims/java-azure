c295dcaecc892ca027fd8ebce836e0505547a3d5
zhiyongli <iamlizhiyong@outlook.com>
2023-09-20 16:25:50 +0800

    Backup

--- stack/ubuntu/generateImage.sh
+++ stack/ubuntu/generateImage.sh
@@ -1,4 +1,4 @@
-docker build . -t showpune/run-debian-base:0.0.1 --target run
-docker push showpune/run-debian-base:0.0.1
-docker build . -t showpune/build-debian-base:0.0.1 --target build
-docker push showpune/build-debian-base:0.0.1
+docker build . -t showpune/run-ubuntu-base:0.0.1 --target run
+docker push showpune/run-ubuntu-base:0.0.1
+docker build . -t showpune/build-ubuntu-base:0.0.1 --target build
+docker push showpune/build-ubuntu-base:0.0.1
