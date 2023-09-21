005a0ce58b21d5b000dc46e578652726bf33a94e
zhiyongli <iamlizhiyong@outlook.com>
2023-09-21 21:39:59 +0800

    Add sample

--- just4build/test.sh
+++ just4build/test.sh
@@ -1,4 +1,5 @@
 ./pack-simple.sh
-pack build showpune/spring-petclinic:buildonly --builder showpune/builder-java-azure-just-build:debian --path /mnt/c/projects/spring-petclinic --env BP_MAVEN_BUILD_ONLY=true -v
+pack build showpune/spring-petclinic:buildonly --builder showpune/builder-java-azure-just-build:debian --path ../sample/source --env BP_MAVEN_BUILD_ONLY=true -v
 docker container create --name test01 showpune/spring-petclinic:buildonly
 docker container cp test01:/workspace .
+docker container rm test01
