39463e6bb1ee691d83e17e1fa7c436639f3bdd2e
zhiyongli <iamlizhiyong@outlook.com>
2023-09-22 01:09:58 +0800

    volumn mount

--- just4build/test.sh
+++ just4build/test.sh
@@ -1,5 +1,10 @@
 ./pack-simple.sh
-pack build showpune/spring-petclinic:buildonly --builder showpune/builder-java-azure-just-build:debian --path ../sample/source --env BP_MAVEN_BUILD_ONLY=true -v
-docker container create --name buildsource showpune/spring-petclinic:buildonly
-docker container cp buildsource:/workspace .
-docker container rm buildsource
+pack build showpune/spring-petclinic:buildonly  --env BP_MAVEN_BUILD_ONLY=true --builder showpune/builder-java-azure-just-build:debian --path ../sample/source --volume /mnt/c/oci/workspace:/tmp:rw -v
+#docker container create --name buildsource showpune/spring-petclinic:buildonly
+#docker container cp buildsource:/workspace .
+#docker container rm buildsource
+
+#pack build showpune/spring-petclinic:buildonly  --env BP_MAVEN_BUILD_ONLY=true --builder showpune/builder-java-azure-just-build:debian --path ../sample/source -v
+
+#pack build showpune/spring-petclinic:buildonly   --path ../sample/source -v
+
