bcc9c179620cbf97268f90de595e021b0d8e976b
Yi Li <yili7@microsoft.com>
2023-09-28 18:56:48 +0800

    refine test

--- pack.sh
+++ pack.sh
@@ -1,8 +1,5 @@
-pack buildpack package showpune/buildpack-java-azure:latest --config ./package.toml
-docker push showpune/buildpack-java-azure:latest
-pack builder create showpune/builder-java-azure:jammy --config ./builder.toml
-docker push showpune/builder-java-azure:jammy
-
-
-pack builder create showpune/builder-java-azure:debian --config ./builder.toml
-docker push showpune/builder-java-azure:debian
+VERSION=0.0.1
+pack buildpack package oryx/java-azure:$VERSION --config ./package.toml --pull-policy always
+BUILDER_VERSION=0928.1
+pack builder create oryx/builder-java-azure:$BUILDER_VERSION --config ./builder.toml --pull-policy always
+pack build app/demo:0.0.18 --builder oryx/builder-java-azure:$BUILDER_VERSION  --path /mnt/c/source/demo --env BP_JAVA_VERSION=17 -v --pull-policy always
--- test.sh
+++
@@ -1,4 +0,0 @@
-pack build showpune/spring-petclinic:mybp --builder showpune/builder-java-azure:debian --path /mnt/c/projects/spring-petclinic -v
-docker run --rm -p 8080:8080 showpune/spring-petclinic:mybp-debian
-
-pack rebase showpune/spring-petclinic:mybp-debian 
